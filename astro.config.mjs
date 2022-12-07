153d9c4b6b315de8cd832f71efdfb5ed743c90be
Fitra Rahman <39044004+Rizki36@users.noreply.github.com>
2022-12-07 23:52:37 +0700

    refactor : change config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://rizki36.github.io",
-  base: "/KomunitasTeknologi.github.io",
+  site: "https://komunitas.devfitra.com",
+  base: "/",
 });
